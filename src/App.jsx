595f00cb0dbde24bfcc8db03b5330fd573ccdcfb
judaargu <judaargu@hotmail.com>
2023-09-28 18:19:03 -0500

    Nav bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,56 @@
-import Home from './Components/Home'
-import { About } from './Components/About'
+import Home from "./Components/Home";
+import { About } from "./Components/About";
+import Style from "./App.module.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [fixed, setFixed] = useState(false);
+
+  useEffect(() => {
+    const adjustNavBar = () => {
+      if (window.scrollY > 0) {
+        setFixed(true);
+        
+      } else {
+        setFixed(false);
+      }
+    };
+    // Escuchamos el evento de scroll cuando el componente se monta
+    window.addEventListener('scroll', adjustNavBar);
+
+    // Eliminamos el evento de scroll cuando el componente se desmonta
+    return () => {
+      window.removeEventListener('scroll', adjustNavBar);
+    };
+   }, []);
 
   return (
-    <div>
-      <div>
-        <ul>
-          <li><a href="#home">Inicio</a></li>
-          <li><a href="#about">Acerca de mi</a></li>
-          <li><a href="">Proyectos</a></li>
-          <li><a href="">Tecnologías</a></li>
-          <li><a href="">Contacto</a></li>
-        </ul>
+    <div >
+      <div className={` ${Style.navBar} ${fixed ? Style.navBar_fixed : ""}`} >
+        <span>
+          <a href="#home">Inicio</a>
+        </span>
+        <span>
+          <a href="#about">Acerca de mi</a>
+        </span>
+        <span>
+          <a href="">Proyectos</a>
+        </span>
+        <span>
+          <a href="">Tecnologías</a>
+        </span>
+        <span>
+          <a href="">Contacto</a>
+        </span>
       </div>
-      <div id='home'>
+      <div id="home">
         <Home />
       </div>
-      <div id='about'>
+      <div id="about">
         <About />
       </div>
-    </div>    
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,15 +1,16 @@
 import React from "react";
+import Style from "./Styles/Home.module.css"
 
 export default function Home() {
   return (
-    <div>
+    <div className={Style.home}>
       <h1>Hola! Soy FullStack developer</h1>
       <br />
       <p>
         Bienvenido a mi página web. Aquí encontrarás información sobre mí;
         formación profesional, proyectos en los que he trabajado, tecnologías y
         demás.
-        Te invito a continuar para conocer más sobre mí y mi trabajo. 
+        Te invito a continuar para conocer más sobre mí y mi trabajo.  
       </p>
       <br />
       <button>Botón para algo</button>
